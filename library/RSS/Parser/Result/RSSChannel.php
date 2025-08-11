d8026d6f9f9a4708a3b0314f2ac06610ac5d182a
Alexander Rieß <alexander.riess@icinga.com>
2025-08-11 09:14:36 +0200

    Make sure attributes are initialized to null

--- library/RSS/Parser/Result/RSSChannel.php
+++ library/RSS/Parser/Result/RSSChannel.php
@@ -4,9 +4,9 @@ namespace Icinga\Module\RSS\Parser\Result;
 
 class RSSChannel
 {
-    public ?string $title;
-    public ?string $link;
+    public ?string $title = null;
+    public ?string $link = null;
     public ?string $image = null;
-    public ?string $description;
+    public ?string $description = null;
     public array $items = [];
 }
--- library/RSS/Parser/Result/RSSItem.php
+++ library/RSS/Parser/Result/RSSItem.php
@@ -4,10 +4,10 @@ namespace Icinga\Module\RSS\Parser\Result;
 
 class RSSItem
 {
-    public ?string $title;
-    public ?string $link;
-    public ?string $description;
+    public ?string $title = null;
+    public ?string $link = null;
+    public ?string $description = null;
     public array $categories = [];
-    public ?string $creator;
-    public ?string $image;
+    public ?string $creator = null;
+    public ?string $image = null;
 }
