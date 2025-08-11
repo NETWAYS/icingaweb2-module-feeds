94df972fb32ecc15853981561413b0daa8e71077
Alexander Rieß <alexander.riess@icinga.com>
2025-08-11 14:05:01 +0200

    Add feed creation form

--- application/controllers/FeedController.php
+++ application/controllers/FeedController.php
@@ -4,7 +4,10 @@ namespace Icinga\Module\RSS\Controllers;
 
 use Icinga\Module\RSS\RSSReader;
 use Icinga\Module\RSS\Web\Item;
+use Icinga\Module\RSS\Forms\CreateFeedForm;
+use Icinga\Module\RSS\Storage\Filesystem;
 
+use Icinga\Web\Notification;
 use ipl\Html\Attributes;
 use ipl\Html\HtmlElement;
 use ipl\Web\Compat\CompatController;
@@ -64,5 +67,33 @@ class FeedController extends CompatController
         $this->addContent($list);
 
         $this->setAutorefreshInterval(5);
+    }
+
+    public function createAction(): void
+    {
+        /* $this->assertPermission('rss/create'); */
+
+        $title = $this->translate('Create a new Feed');
+        $this->setTitle($title);
+
+        $storage = new Filesystem();
+        $form = new CreateFeedForm($storage);
+
+        /* $formData = [ */
+        /*     'name' => $this->params->get('name'), */
+        /*     'url' => $this->params->get('url'), */
+        /* ]; */
+        /* $form->populate($formData); */
+
+        $form->on(CreateFeedForm::ON_SUCCESS, function () {
+            $this->redirectNow('__CLOSE__');
+        });
+
+        $form->handleRequest($this->getServerRequest());
+
+        $this->addContent($form);
+
+        /* $this->displayError('TODO: Implement form'); */
+        /* return; */
     }
 }
---
+++ application/forms/CreateFeedForm.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Icinga\Module\RSS\Forms;
+
+use Icinga\Web\Notification;
+use ipl\Sql\Connection;
+use ipl\Web\Compat\CompatForm;
+use Icinga\Module\RSS\Storage\StorageInterface;
+use Icinga\Module\RSS\Storage\FeedDefinition;
+
+class CreateFeedForm extends CompatForm
+{
+    public function __construct(
+        protected StorageInterface $storage,
+    ) {}
+
+    protected function assemble(): void
+    {
+        // TODO: Add validation
+        $this->addElement('text', 'name', [
+            'label'      => $this->translate('Name'),
+            'required'   => true,
+            'description' => $this->translate(
+                'This is the unique identifier of this process'
+            ),
+        ]);
+
+        $this->addElement('text', 'url', [
+            'label'       => $this->translate('URL'),
+            'required'    => true,
+            'description' => $this->translate('The URL to the feed'),
+        ]);
+
+        /* $this->addElement('textarea', 'Description', array( */
+        /*     'label'       => $this->translate('Description'), */
+        /*     'description' => $this->translate( */
+        /*         'A slightly more detailed description for this process, about 100-150 characters long' */
+        /*     ), */
+        /*     'rows' => 4, */
+        /* )); */
+        /**/
+        /* $this->addElement('select', 'Statetype', array( */
+        /*     'label'       => $this->translate('State Type'), */
+        /*     'required'    => true, */
+        /*     'description' => $this->translate( */
+        /*         'Whether this process should be based on Icinga hard or soft states' */
+        /*     ), */
+        /*     'multiOptions' => array( */
+        /*         'soft' => $this->translate('Use SOFT states'), */
+        /*         'hard' => $this->translate('Use HARD states'), */
+        /*     ) */
+        /* )); */
+
+        /* $label = $this->translate('Delete'); */
+        /* $el = $this->createElement( */
+        /*     'submit', */
+        /*     $label, */
+        /*     [ */
+        /*         'data-base-target' => '_main', */
+        /*     ]) */
+        /*     ->setLabel($label); */
+        /* $this->deleteButtonName = $el->getName(); */
+        /* $this->addElement($el); */
+
+        $this->addElement('submit', 'submit', [
+            'label' => 'Submit'
+        ]);
+    }
+
+    protected function onSuccess()
+    {
+        $name = $this->getValue('name');
+        $url = $this->getValue('url');
+
+        $feed = new FeedDefinition($name, $url);
+
+        if ($this->storage->getFeedByName($name) !== null) {
+            Notification::error("A feed with the name {$name} already exsits");
+            return;
+        }
+
+        $this->storage->addFeed($feed);
+    }
+}
