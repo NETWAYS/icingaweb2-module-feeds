dff846a550e09cc3530cce7d5f6147fb3c695cd2
Alexander Rieß <alexander.riess@icinga.com>
2025-08-12 09:27:53 +0200

    Add mixed feed view

--- application/controllers/FeedsController.php
+++ application/controllers/FeedsController.php
@@ -2,20 +2,75 @@
 
 namespace Icinga\Module\RSS\Controllers;
 
+use Icinga\Module\RSS\RSSReader;
 use Icinga\Module\RSS\Storage\Filesystem;
-
 use Icinga\Module\RSS\Web\Table;
+use Icinga\Module\RSS\Web\Item;
 
 use ipl\Html\Attributes;
 use ipl\Html\HtmlElement;
 use ipl\Web\Compat\CompatController;
 use ipl\Web\Widget\Link;
 
+use \Exception;
+
 class FeedsController extends CompatController
 {
     public function indexAction(): void
     {
         $title = $this->translate('Feeds');
+        $this->addControl(
+            HtmlElement::create('h1', null, $title)
+        );
+        $this->setTitle($title);
+
+        $storage = new Filesystem();
+
+        $limit = $this->params->shift('limit') ?? 50;
+        $compact = ($this->params->shift('view') ?? 'minimal') === 'minimal';
+
+        $items = [];
+        foreach ($storage->getFeeds() as $feed) {
+            try {
+                $reader = new RSSReader($feed->url);
+                $data = $reader->fetch();
+            } catch (Exception $ex) {
+                // TODO: Figure out a way to display the error
+                continue;
+            }
+
+            $items = array_merge($items, $data->getItems());
+        }
+
+        // Sort items
+        usort($items, function($a, $b) {
+            return -($a->compareDate($b));
+        });
+
+        $index = 0;
+        $elements = [];
+        foreach ($items as $item) {
+            $elements[] = new Item($item, $compact);
+            $index++;
+            if ($index > $limit) {
+                break;
+            }
+        }
+
+        $list = HtmlElement::create(
+            'ul',
+            Attributes::create(['class' => 'feed-items']),
+            $elements
+        );
+
+        $this->addContent($list);
+
+        $this->setAutorefreshInterval(300);
+    }
+
+    public function listAction(): void
+    {
+        $title = $this->translate('List Feeds');
         $this->addControl(
             HtmlElement::create('h1', null, $title)
         );
