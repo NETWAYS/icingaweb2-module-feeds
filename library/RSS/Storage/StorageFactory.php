4e32cd4b65b79d4ff075f29ba904a4393556bdeb
Alexander Rieß <alexander.riess@icinga.com>
2025-08-13 13:07:30 +0200

    Reuse the same StorageInterface for the whole request

--- library/RSS/Storage/StorageFactory.php
+++ library/RSS/Storage/StorageFactory.php
@@ -4,10 +4,20 @@ namespace Icinga\Module\RSS\Storage;
 
 class StorageFactory
 {
-    public static function getStorage(): StorageInterface
+    protected static ?StorageInterface $cache = null;
+
+    protected static function getStorageImpl(): StorageInterface
     {
         // TODO: Once there are multiple storage options they can be determined
         // from the config file
         return new Filesystem();
+    }
+
+    public static function getStorage(): StorageInterface
+    {
+        if (static::$cache === null) {
+            static::$cache = static::getStorageImpl();
+        }
+        return static::$cache;
     }
 }
